92bbbe1a9edf62fbf76e77dcffacd063470b295a
John David Calimpong <johndavid.calimpong@cit.edu>
2026-01-26 17:19:54 +0800

    Fresh Strapi Setup (Docker + Postgres)

---
+++ strapi-backend/config/database.ts
@@ -0,0 +1,24 @@
+export default ({ env }) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: env('DATABASE_HOST', '127.0.0.1'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'strapi'),
+      user: env('DATABASE_USERNAME', 'strapi'),
+      password: env('DATABASE_PASSWORD', 'strapi'),
+      ssl: env.bool('DATABASE_SSL', false),
+    },
+    debug: false,
+    pool: {
+      min: 2,
+      max: 10,
+      acquireTimeoutMillis: 60000,
+      createTimeoutMillis: 30000,
+      destroyTimeoutMillis: 5000,
+      idleTimeoutMillis: 30000,
+      reapIntervalMillis: 1000,
+      createRetryIntervalMillis: 200,
+    },
+  },
+});
